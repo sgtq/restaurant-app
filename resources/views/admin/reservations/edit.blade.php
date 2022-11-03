ec61051c15621d78f4ab66a9f10b4488c82e2804
Gaston <gaston.quittner@gmail.com>
2022-11-02 19:01:51 -0600

    Index actions, edit and delete for all controllers. - Tables - Reservations - Menus

---
+++ resources/views/admin/reservations/edit.blade.php
@@ -0,0 +1,102 @@
+<x-admin-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Edit Reservation') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="flex justify-end m-2 p-2">
+                <a href="{{ route('admin.reservations.index') }}"
+                   class="px-4 py-2 bg-indigo-500 hover:bg-indigo-700 rounded-lg text-white">Reservations Index</a>
+            </div>
+
+            <div class="m-2 p-2 bg-slate-100 rounded">
+                <div class="space-y-8 divide-y divide-gray-200 w-1/2 mt-10">
+                    <form method="POST" action="{{ route('admin.reservations.update', $reservation->id) }}" enctype="multipart/form-data">
+                        @csrf
+                        @method('PUT')
+                        <div class="sm:col-span-6">
+                            <label for="first_name" class="block text-sm font-medium text-gray-700"> First Name </label>
+                            <div class="mt-1">
+                                <input type="text" id="first_name" name="first_name" value="{{ $reservation->first_name }}"
+                                       class="block w-full appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5 @error('name') border-red-400 @enderror" />
+                            </div>
+                            @error('first_name')
+                            <div class="text-sm text-red-400">{{ $message }}</div>
+                            @enderror
+                        </div>
+                        <div class="sm:col-span-6">
+                            <label for="last_name" class="block text-sm font-medium text-gray-700"> Last Name </label>
+                            <div class="mt-1">
+                                <input type="text" id="last_name" name="last_name" value="{{ $reservation->last_name }}"
+                                       class="block w-full appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5 @error('name') border-red-400 @enderror" />
+                            </div>
+                            @error('last_name')
+                            <div class="text-sm text-red-400">{{ $message }}</div>
+                            @enderror
+                        </div>
+                        <div class="sm:col-span-6">
+                            <label for="email" class="block text-sm font-medium text-gray-700"> Email </label>
+                            <div class="mt-1">
+                                <input type="text" id="email" name="email" value="{{ $reservation->email }}"
+                                       class="block w-full appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5 @error('name') border-red-400 @enderror" />
+                            </div>
+                            @error('email')
+                            <div class="text-sm text-red-400">{{ $message }}</div>
+                            @enderror
+                        </div>
+                        <div class="sm:col-span-6">
+                            <label for="phone" class="block text-sm font-medium text-gray-700"> Phone </label>
+                            <div class="mt-1">
+                                <input type="text" id="phone" name="phone" value="{{ $reservation->phone }}"
+                                       class="block w-full appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5 @error('name') border-red-400 @enderror" />
+                            </div>
+                            @error('phone')
+                            <div class="text-sm text-red-400">{{ $message }}</div>
+                            @enderror
+                        </div>
+                        <div class="sm:col-span-6">
+                            <label for="datetime" class="block text-sm font-medium text-gray-700"> Date </label>
+                            <div class="mt-1">
+                                <input type="datetime-local" id="datetime" name="datetime" value="{{ $reservation->datetime }}"
+                                       class="block w-full appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5 @error('name') border-red-400 @enderror" />
+                            </div>
+                            @error('datetime')
+                            <div class="text-sm text-red-400">{{ $message }}</div>
+                            @enderror
+                        </div>
+                        <div class="sm:col-span-6">
+                            <label for="table_id" class="block text-sm font-medium text-gray-700"> Table </label>
+                            <div class="mt-1">
+                                <input type="text" id="table_id" name="table_id"
+                                       class="block w-full appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5 @error('name') border-red-400 @enderror" />
+                            </div>
+                            @error('table_id')
+                            <div class="text-sm text-red-400">{{ $message }}</div>
+                            @enderror
+                        </div>
+                        <div class="sm:col-span-6">
+                            <label for="guest_number" class="block text-sm font-medium text-gray-700"> Guests </label>
+                            <div class="mt-1">
+                                <input type="number" id="guest_number" name="guest_number" value="{{ $reservation->guest_number }}"
+                                       class="block w-full appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5 @error('name') border-red-400 @enderror" />
+                            </div>
+                            @error('guest_number')
+                            <div class="text-sm text-red-400">{{ $message }}</div>
+                            @enderror
+                        </div>
+                        <div class="mt-6 p-4">
+                            <button type="submit"
+                                    class="px-4 py-2 bg-indigo-500 hover:bg-indigo-700 rounded-lg text-white">
+                                Save
+                            </button>
+                        </div>
+                    </form>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</x-admin-layout>
---
+++ resources/views/admin/reservations/index.blade.php
@@ -0,0 +1,94 @@
+<x-admin-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Reservations') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="flex justify-end m-2 p-2">
+                <a href="{{ route('admin.reservations.create') }}" class="px-4 py-2 bg-indigo-500 hover:bg-indigo-700 rounded-lg text-white">
+                    New Reservation
+                </a>
+            </div>
+
+            <div class="flex flex-col">
+                <div class="overflow-x-auto relative">
+                    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                            <tr>
+                                <th scope="col" class="py-3 px-6">
+                                    First Name
+                                </th>
+                                <th scope="col" class="py-3 px-6">
+                                    Last Name
+                                </th>
+                                <th scope="col" class="py-3 px-6">
+                                    Email
+                                </th>
+                                <th scope="col" class="py-3 px-6">
+                                    Phone
+                                </th>
+                                <th scope="col" class="py-3 px-6">
+                                    Guests
+                                </th>
+                                <th scope="col" class="py-3 px-6">
+                                    Date
+                                </th>
+                                <th scope="col" class="py-3 px-6">
+                                    TABLE ?????
+                                </th>
+                                <th scope="col" class="relative py-3 px-6">
+                                    <span class="sr-only">Actions</span>
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach( $reservations as $reservation )
+                            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                                <td class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                    {{ $reservation->first_name }}
+                                </td>
+                                <td class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                    {{ $reservation->last_name }}
+                                </td>
+                                <td class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                    {{ $reservation->email }}
+                                </td>
+                                <td class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                    {{ $reservation->phone }}
+                                </td>
+                                <td class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                    {{ $reservation->guests_number }}
+                                </td>
+                                <td class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                    {{ $reservation->date }}
+                                </td>
+                                <td class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                    {{ $reservation->table_id }}
+                                </td>
+                                <td class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                    <div class="flex space-x-2">
+                                        <a href="{{ route('admin.reservations.edit', $reservation->id) }}" class="px-4 py-2 bg-green-500 hover:gb-green-700 rounded-lg text-white">
+                                            Edit
+                                        </a>
+                                        <form class="px-4 py-2 bg-red-500 hover:gb-red-700 rounded-lg text-white"
+                                              method="POST"
+                                              action="{{ route('admin.reservations.destroy', $reservation->id) }}"
+                                              onsubmit="return confirm('Are you sure you want to delete reservation for {{ $reservation->last_name }}?');">
+                                            @csrf
+                                            @method('DELETE')
+                                            <button type="submit">Delete</button>
+                                        </form>
+                                    </div>
+                                </td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-admin-layout>
