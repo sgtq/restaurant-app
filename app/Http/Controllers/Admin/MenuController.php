b7eb38866ef89599ddf3a0ff2df5d4462531c527
Gaston <gaston.quittner@gmail.com>
2022-11-02 19:12:19 -0600

    Pivot Table for Category <-> Menu. - add belongsToMany between category and menu.

--- app/Http/Controllers/Admin/MenuController.php
+++ app/Http/Controllers/Admin/MenuController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\CategoryStoreRequest;
+use App\Models\Category;
 use App\Models\Menu;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class MenuController extends Controller
 {
@@ -26,7 +29,9 @@ class MenuController extends Controller
      */
     public function create()
     {
-        return view('admin.menus.create');
+        $categories = Category::all(['id', 'name']);
+
+        return view('admin.menus.create', compact('categories'));
     }
 
     /**
@@ -35,7 +40,7 @@ class MenuController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(MenuStoreRequest $request)
     {
         //
     }
@@ -57,9 +62,9 @@ class MenuController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(Menu $menu)
     {
-        //
+        return view('admin.menus.edit', compact('menu'));
     }
 
     /**
@@ -69,9 +74,25 @@ class MenuController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, Menu $item)
     {
-        //
+        $request->validate([
+            'name' => 'required',
+            'description' => 'required'
+        ]);
+
+        if ($request->hasFile('image')) { // Check if the file has anything to change to
+            Storage::delete($item->image); // Remove existing image from files
+            $image = $request->file('image')->store('public/menus');
+        }
+
+        $item->update([
+            'name' => $request->name,
+            'description' => $request->description,
+            'image' => $image
+        ]);
+
+        return to_route('admin.menus.index');
     }
 
     /**
@@ -80,8 +101,10 @@ class MenuController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Menu $item)
     {
-        //
+        Storage::delete($item->image);
+        $item->delete();
+        return to_route('admin.menus.index');
     }
 }
--- resources/views/admin/menus/create.blade.php
+++ resources/views/admin/menus/create.blade.php
@@ -50,16 +50,26 @@
                             <label for="price" class="block text-sm font-medium text-gray-700">Price</label>
                             <div class="mt-1">
                                 <input type="number" id="price" rows="3" name="price"
-                                          class="shadow-sm focus:ring-indigo-500 appearance-none bg-white border py-2 px-3 text-base leading-normal transition duration-150 ease-in-out focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md @error('name') border-red-400 @enderror"></input>
-                            </div>
+                                          class="shadow-sm focus:ring-indigo-500 appearance-none bg-white border py-2 px-3 text-base leading-normal transition duration-150 ease-in-out focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md @error('name') border-red-400 @enderror" />
                             @error('price')
                             <div class="text-sm text-red-400">{{ $message }}</div>
                             @enderror
                         </div>
+                        <div class="sm:col-span-6">
+                            <label for="name" class="block text-sm font-medium text-gray-700"> Categories </label>
+                            <div class="mt-1">
+                                <select multiple
+                                        class="shadow-sm focus:ring-indigo-500 appearance-none bg-white border py-2 px-3 text-base leading-normal transition duration-150 ease-in-out focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md @error('name') border-red-400 @enderror">
+                                    @foreach ($categories as $category)
+                                        <option value="{{ $category->id }}">{{ $category->name }}</option>
+                                    @endforeach;
+                                </select>
+                            </div>
+                        </div>
                         <div class="mt-6 p-4">
                             <button type="submit"
                                     class="px-4 py-2 bg-indigo-500 hover:bg-indigo-700 rounded-lg text-white">
-                                Store
+                                Save
                             </button>
                         </div>
                     </form>
