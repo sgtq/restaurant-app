bea0526ff1448af42c03533a277b76e39e3d347d
Gaston <gaston.quittner@gmail.com>
2022-11-07 17:32:34 -0600

    Create Routes -

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,9 @@ use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\MenuController;
 use App\Http\Controllers\Admin\TableController;
 use App\Http\Controllers\Admin\ReservationController;
+use App\Http\Controllers\Frontend\CategoryController as FrontendCategoryController;
+use App\Http\Controllers\Frontend\MenuController as FrontendMenuController;
+use App\Http\Controllers\Frontend\ReservationController as FrontendReservationController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,6 +24,12 @@ use App\Http\Controllers\Admin\ReservationController;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/categories', [FrontendCategoryController::class, 'index'])->name('categories.index');
+Route::get('/categories/{category}', [FrontendCategoryController::class, 'show'])->name('categories.show');
+Route::get('/menus', [FrontendMenuController::class, 'index'])->name('menus.index');
+Route::get('/reservation/step-one', [FrontendReservationController::class, 'stepOne'])->name('reservations.stepOne');
+Route::get('/reservation/step-two', [FrontendReservationController::class, 'stepTwo'])->name('reservations.stepTwo');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
